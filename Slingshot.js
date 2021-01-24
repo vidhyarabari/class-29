1f7ff20d774bd6afbce3650b7ca1d5a070b6124f
vidhyarabri <vidhyasr2007@gmail.com>
2021-01-24 12:44:09 +0530

    added cattapult

--- Slingshot.js
+++ Slingshot.js
@@ -9,6 +9,10 @@ class SlingShot{
         this.pointB = pointB
         this.sling = Constraint.create(options);
         World.add(world, this.sling);
+        this.sling1=loadImage("sprites/sling1.png")
+        this.sling2=loadImage("sprites/sling2.png")
+        this.sling3=loadImage("sprites/sling3.png")
+
     }
 
     fly(){
@@ -16,12 +20,29 @@ class SlingShot{
     }
 
     display(){
+        
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            stroke(48,22,8);
+            if(pointA.x<230){
+            strokeWeight(8);
+
+            line(pointA.x-20, pointA.y, pointB.x-20, pointB.y);
+            line(pointA.x-20,pointA.y,pointB.x+20,pointB.y);
+            image(this.sling3,pointA.x-25,pointA.y,15,15);
+            }
+            else {
+                strokeWeight(5);
+
+                line(pointA.x+25, pointA.y, pointB.x-20, pointB.y);
+                line(pointA.x+25,pointA.y,pointB.x+20,pointB.y);
+                image(this.sling3,pointA.x+25,pointA.y-5,15,15);
+
+            }
         }
+        image(this.sling1,200,20);
+        image(this.sling2,170,20);
     }
     
 }
